4d02a1e0635e56731cc3365b42959710c6f97d98
guinness30 <passmeby30@outlook.com>
2023-12-02 16:12:30 +0900

    change hellowordl msg

--- src/main/java/com/example/cloudnative/CloudnativeApplication.java
+++ src/main/java/com/example/cloudnative/CloudnativeApplication.java
@@ -13,7 +13,7 @@ public class CloudnativeApplication {
 
 	@RequestMapping("/")
 	String home() {
-		return "Hello World!!!!!!";
+		return "Hello World!fadfdsa!!!!!";
 	}
 	
 	public static void main(String[] args) {
